e0dc6a126cd02c3a07704c6e45e976fcba5aea18
adly125 <adly_125@hotmail.com>
2019-11-07 10:26:50 +0100

    comentario de texto

--- 07-formularios.php
+++ 07-formularios.php
@@ -10,9 +10,18 @@
 	$varnom=$_GET{"nome"}??"";
 	$varape=$_GET{"apelidos"}??"";
 	$varclave=$_GET{"clave"}??"";
+	$varsexo=$_GET{"sexo"}??"";
+	$varxornada=$_GET{"xornada"}??"";
+	$varfutbol=$_GET{"futbol"}??"";
+	$vartenis=$_GET{"tenis"}??"";
+	$varref=$_GET{"ref"}??"";
 	echo "<p>nome: $varnom</p>";
 	echo "<p>apelidos: $varape</p>";
 	echo "<p>Contrasinal: $varclave</p>";
+	echo "<p>Sexo: $varsexo</p>";
+	echo "<p>Futbol $varfutbol</p>";
+	echo "<p>Tenis $vartenis</p>";
+	echo "<p>Referencia (oculta): $varref</p>";
 	?>
 </body>
 </html>
